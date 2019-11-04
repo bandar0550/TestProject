a0ac30b0439be57945ff63222bbd062dae321fb3
user <user@DESKTOP-6KM06LE>
2019-11-04 10:19:48 -0800

    annotations

---
+++ spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
@@ -0,0 +1,14 @@
+package com.luv2code.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component("thatSilly")
+public class TennisCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		
+		return "Practice you backhand volley";
+	}
+
+}
